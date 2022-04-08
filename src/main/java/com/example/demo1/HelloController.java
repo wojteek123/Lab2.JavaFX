1350d74ed4dc0a2f30d7f78a3b9dad204882c6d9
wojtek123 <1889wojtek@gmail.com>
2022-04-08 22:42:45 +0200

    Lab2.JavaFX - test Dodanie 3x TextField

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -2,20 +2,29 @@ package com.example.demo1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
     @FXML
+    /*
     private Label counter;
     public Button button;
     public int count=0;
+    */
+    public TextField w_height;
+    public TextField w_radius;
+    public TextField w_grubosc;
+
+
+
     @FXML
     protected void onHelloButtonClick() {
-        count+=1;
-        String s=Integer.toString(count);
-        counter.setText(s);
-        button.setText("I jescze raz");
+
+        //count+=1;
+        //String s=Integer.toString(count);
+        //counter.setText(s);
+        //button.setText("I jescze raz");
+
     }
 
 }
