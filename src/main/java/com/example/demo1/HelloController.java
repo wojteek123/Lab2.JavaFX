1d4863786760ce90d998d285e0844145b55332d7
wojtek123 <1889wojtek@gmail.com>
2022-04-09 19:39:58 +0200

    Lab2.JavaFX obliczanie i wyswietlanie wagi oraz ilosci potrzebnego materiału

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -4,6 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+
 public class HelloController {
     @FXML
     /*
@@ -17,14 +20,21 @@ public class HelloController {
     public TextArea results;
     public Label error;
     public Label error2;
+    public CheckBox corrosion;
     public void initialize() {
         error.setVisible(false);
         error2.setVisible(false);
+    }
+    public double shnumber(String numberString){
+        int indexOfDecimal = numberString.indexOf('.');
+        double truncatedNumber = Double.parseDouble(numberString.substring(0, indexOfDecimal + 3));
+        return truncatedNumber;
     }
 
 
     @FXML
     protected void onHelloButtonClick() {
+
         String ws_height = w_height.getText();
         String ws_radius = w_radius.getText();
         String ws_grubosc = w_grubosc.getText();
@@ -39,13 +49,57 @@ public class HelloController {
                 return;
             }
             error2.setVisible(false);
-
+            String temp="";
+            results.setText(temp);
 
             double V1 = 3.14*(wd_radius*wd_radius)*wd_height;
             double V2 = 3.14*((wd_radius-wd_grubosc)*(wd_radius-wd_grubosc))*(wd_height-(2*wd_grubosc));
+           ///System.out.printf("V1 = "+String.valueOf(V1)+"\n");
+           // System.out.printf("V2 = "+String.valueOf(V2)+"\n");
 
-            System.out.printf("V1 = "+String.valueOf(V1)+"\n");
-            System.out.printf("V2 = "+String.valueOf(V2)+"\n");
+            double steel_amount=V1-V2; //ilosc potrzebnej stali w cm szesciennych
+
+            double weight = steel_amount*7.9; //stal wazy 7.9 gram na cm szescienny
+            weight /= 1000; // na kg
+
+            // Ile waży stal nierdzewna?
+            //1 m2 blachy kwasoodpornej o grubości 1 mm waży około 8 kg.
+            //1 m2 = 10 000 cm2 , 10 000cm2 * 0,1 cm = 1000cm3
+            // 1000cm3 = 8kg ,      1cm3 = 0,008 kg
+            double weight_corrosion=steel_amount*0.008;
+
+            double price_steel=10d;
+            double price_corrosion=16d; //za kg
+
+            //System.out.printf("weight = "+String.valueOf(weight)+"kg \n");
+            //System.out.printf("weight corrosion = "+String.valueOf(weight_corrosion)+"kg \n");
+
+            double pole_b = 2*3.14*wd_radius*wd_height;
+            double pole_p = 2*3.14*wd_radius*wd_radius;
+            double pole_c = pole_b * pole_p*2;
+
+
+
+
+            String textresult= "Ilość potrzebnego materiału: "+shnumber(Double.toString(pole_c/100))+" m2 blachy. \n";
+            String textresult2= "Waga materiału dla stali: "+shnumber(Double.toString(weight))+" kg. \n";
+            String textresult3= "Waga materiału dla stali nierdzewnej: "+shnumber(Double.toString(weight_corrosion))+" kg. \n";
+
+
+
+            //String textresult = "sd";//String.format("Ilość potrzebnego materiału: %.2d m2 blachy. %n",pole_c);
+            //String textresult2 ="asd"; //String.format("asd");
+            //String textresult3 = "asd"; //String.format("asd")
+
+            results.appendText(textresult);
+            results.appendText(textresult2);
+            results.appendText(textresult3);
+
+
+
+
+
+
 
         }else {
             error.setVisible(true);
@@ -63,8 +117,4 @@ public class HelloController {
     public boolean checkTextField(String data) {
             return data != null && data.matches("[0-9.]+");
         }
-    public double volume(double height,double radius){
-        return 1d;
-
-    }
 }
